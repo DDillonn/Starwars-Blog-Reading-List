7f4a2a898cd4906d1da84209b9e3508aa60f5595
DDillonn <Saltnessdillon@gmail.com>
2022-06-05 10:08:47 +0000

    why doesnt this work

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
   <footer className="footer my-3 py-3 text-center">
     <p>
-      Made with <i className="fa fa-heart text-success" /> by{" Dillon Saltness "}
+      Made with <i className="fa fa-heart text-success" /> by{" Dillon Sagitltness "}
       <a href="http://www.4geeksacademy.com">4Geeks Academy</a>
     </p>
   </footer>
